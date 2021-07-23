bc563141441556b5d049e1312f81859caa63e60f
Jackie Do <huydo91@outlook.com>
2021-07-23 11:28:55 +0700

    Add CSS and JS resources

---
+++ app/javascript/packs/bottom_load.js
@@ -0,0 +1,14 @@
+//// IMPORT LIBRARIES
+// import '../libs/jquery.min.js';
+import 'jquery'
+
+
+//// IMPORT VENDORS
+import '../vendors/bootstrap/js/bootstrap.bundle.min';
+import '../vendors/OwlCarousel/owl.carousel';
+import '../vendors/bootstrap_select/js/bootstrap_select';
+
+
+//// IMPORT CUSTOM JS
+import '../src/custom';
+import '../src/night-mode';
